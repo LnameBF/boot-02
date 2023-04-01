1f31f779cc01d318cb59fdebfcc248a96c96937b
Hei <waityou32502@163.com>
2023-04-01 15:41:52 +0800

    Initial commit

---
+++ web01/src/main/java/com/example/web01/controller/helloController.java
@@ -0,0 +1,17 @@
+package com.example.web01.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class helloController {
+
+    @GetMapping
+    public String GetHello(){
+        System.out.println("hello world");
+        return "hello world";
+    }
+}
